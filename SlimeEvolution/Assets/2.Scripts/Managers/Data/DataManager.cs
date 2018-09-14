ed82fc8bdd55b2584e8b0d9bf2cdd14016165090
박준명 <ggolnam3@gmail.com>
2018-09-14 14:58:30 +0900

    박준명_Unity프로젝트 폴더구조 설계 및 서버 연동 완료.

---
+++ SlimeEvolution/Assets/2.Scripts/Managers/Data/DataManager.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+namespace SlimeEvolution.GameSystem
+{
+    
+    /// <summary>
+    /// 작성자 : 박준명
+    /// </summary>
+    public class DataManager : Singleton<DataManager>
+    {
+        public const string DirectoryName = "Data";
+        public const string FilePath = DirectoryName + "/AssetBundleData.txt";
+        public delegate void LoginResult(string Text);
+        public LoginResult LoginResultCallback;
+        public delegate void SignUpResult(string Text);
+        public LoginResult SignUpResultCallback;
+
+        private GameData gameData;
+
+        public GameData GameData
+        {
+            get { return gameData; }
+        }
+
+        private GameDataLoader gameDataLoader;
+        // Use this for initialization
+        void Awake()
+        {
+            gameData = new GameData();
+            gameDataLoader = new GameDataLoader();
+            CreateNewAccount("jun", "353535");
+            DontDestroyOnLoad(gameObject);
+        }
+
+        public void CreateNewAccount(string id, string password)
+        {
+            gameDataLoader.MakeNewAccountInDB(id, password);
+        }
+
+        public void Login(string id, string password)
+        {
+            gameDataLoader.LoadLoginDataFromDB(id, password);
+        }
+
+        public void SaveGameData(GameData gameData, bool ShouldSaveForDB)
+        {
+            this.gameData = gameData;
+
+            if (ShouldSaveForDB)
+            {
+                gameDataLoader.SetUpdateInDB(gameData);
+            }
+        }
+
+        public void ResetData()
+        {
+            gameData.PlayerData = new PlayerData();
+            gameDataLoader.SetUpdateInDB(gameData);
+        }
+
+        /// <summary>
+        /// AssetBundle의 키 값을 비교하여 업데이트 유무를 Bool형식으로 리턴함.(사용자는 꼭 Enum순서대로 함수를 사용할것. 
+        /// ex) Player -> Skill(o) , Player -> Enemy(x))
+        /// </summary>
+        /// <param name="bundleNumbers"></param>
+        /// <param name="assetBundleData"></param>
+        /// <returns></returns>
+        //public bool CheckedBundleVersion(AssetBundleNumbers bundleNumbers, string assetBundleData)
+        //{
+        //    if (!Directory.Exists(DirectoryName))
+        //    {
+        //        Directory.CreateDirectory(DirectoryName);
+        //        LogManager.Instance.UserDebug(LogColor.Magenta, GetType().Name, "AssetBundle을 위한 Data폴더 생성");
+        //    }
+
+        //    FileInfo fileInfo = new FileInfo(FilePath);
+        //    if(!fileInfo.Exists)
+        //    {
+        //        File.Create(FilePath).Dispose();
+        //    }
+        //    string[] lines;
+        //    lines = File.ReadAllLines(FilePath);
+        //    if (lines.Length - 1 < (int)bundleNumbers)
+        //    {
+        //        using (StreamWriter writer = new StreamWriter(FilePath, true))
+        //        {
+        //            writer.WriteLine(assetBundleData);
+        //            LogManager.Instance.UserDebug(LogColor.Magenta, GetType().Name, "기록된 정보가 없음. 새로 입력.");   
+        //        }
+        //    return false;
+        //    }
+        //    else if (!lines[(int)bundleNumbers].Equals(assetBundleData))
+        //    {
+        //        using (StreamWriter writer = new StreamWriter(FilePath, false))
+        //        {
+        //            lines[(int)bundleNumbers] = assetBundleData;
+        //            for (int i = 0; i < lines.Length; i++)
+        //                writer.WriteLine(lines[i]);
+        //            LogManager.Instance.UserDebug(LogColor.Magenta, GetType().Name, "기록된 정보와 불일치. 업데이트 필요.");
+        //        }
+        //    return false;
+        //    }
+        //    LogManager.Instance.UserDebug(LogColor.Magenta, GetType().Name, "기록된 정보와 일치. 업데이트 필요없음");
+        //    return true;
+        //}
+
+        //[Serializable]
+        //public struct AssetBundleData
+        //{
+        //    public string Player;
+        //    public string Skill;
+        //    public string Enemey;
+        //    public string FirstBoss;
+        //    public string SecondBoss;
+        //    public string ThirdBoss;
+        //    public string UI;
+        //    public string Tile;
+        //}
+    }
+
+}
+
+
---
+++ SlimeEvolution/Assets/2.Scripts/Managers/Data/GameData.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace SlimeEvolution.GameSystem
+{
+    /// <summary>
+    /// 작성자 : 박준명
+    /// </summary>
+    [Serializable]
+    public class PlayerData
+    {
+        string Name = "Jun";
+        int Level = 1;
+        int Hp = 50;
+        int Mp = 30;
+        int AD = 15;
+        int DEF = 4;
+    }
+
+    /// <summary>
+    /// 작성자 : 박준명
+    /// </summary>
+    [Serializable]
+    public class GameData
+    {
+        public string UserId;
+        public string UserPassword;
+        public PlayerData PlayerData = new PlayerData();
+
+    }
+}
+
